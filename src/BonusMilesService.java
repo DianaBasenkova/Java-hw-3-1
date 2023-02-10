4d42a6223b1c60cb4854185d1e872cd87848ee22
Diana Basenkova <dinnight123@yandex.ru>
2023-02-10 13:31:40 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+       // int mile = 20;
+        int calcMiles = (price / 20);//=(price/mile);
+        return calcMiles;
+    }
+}
